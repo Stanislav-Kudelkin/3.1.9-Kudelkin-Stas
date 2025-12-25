1d77bfc2cbc9e63feaa8270e00d75d603a8f05fa
Станислав Куделькин <stanislavkudelkin@MacBook-Air-Stanislav.local>
2025-12-25 20:20:21 +0500

    debugged the interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { useState, ChangeEvent, memo, useMemo } from 'react';
-import { generateProducts, filterProducts } from './utils.ts';
-import { ProductList } from './ProductList';
-import './App.css';
+import { useState, ChangeEvent, memo, useMemo, useTransition } from 'react'
+import { generateProducts, filterProducts } from './utils.ts'
+import { ProductList } from './ProductList'
+import './App.css'
 
 export const App = memo(() => {
-  const [filterTerm, setFilterTerm] = useState('');
+  const [isPending, startTransition] = useTransition()
+  const [filterTerm, setFilterTerm] = useState('')
 
-  const dummyProducts = useMemo(() => generateProducts(), []);
-  const filteredProducts = filterProducts(filterTerm, dummyProducts);
+  const dummyProducts = useMemo(() => generateProducts(), [])
+  const filteredProducts = filterProducts(filterTerm, dummyProducts)
 
   const updateFilterHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setFilterTerm(event.target.value);
-  };
+    startTransition(() => {
+      setFilterTerm(event.target.value)
+    })
+  }
 
   return (
     <>
       <input className="input" type="text" onChange={updateFilterHandler} />
-      <ProductList products={filteredProducts} />
+      {isPending ? (
+        <div className="loader"></div>
+      ) : (
+        <ProductList products={filteredProducts} />
+      )}
     </>
-  );
-});
+  )
+})
